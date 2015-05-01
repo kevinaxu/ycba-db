28c5a608464b41b51451909f2855f49b2352b35d
Kevin Xu <kevin.xu@yale.edu>
2015-05-01 03:08:25 -0400

    Create setup script for installing all necessary drivers.

---
+++ setup.py
@@ -0,0 +1,40 @@
+import sys
+
+def main():
+
+	# MANUAL: download wamp server and default install 
+	# Download wamp drivers to get rid of error message 
+
+	# Download the repository from github
+	# Unpack and rename to 'ycba'
+	# Rename app.ini.example to app.ini
+	# Add credentials into app.ini 
+
+	# Confirm that wamp server installed correctly
+	# Move the ycba directory into c:/wamp/www
+
+	# Download the php sqlsrv drivers 
+	# Put them into c:/wamp/bin/php/php5.5/ext
+	# Add extension information into php.ini 
+
+	# Download the drivers for sql server 
+	# MANUAL: Run the executable 
+
+	# Restart wamp server and confirm that it's working 
+
+
+
+
+	# resources
+	#	-> drivers 
+	#		-> php_sqlsrv
+	#			-> ts.dll
+	#			-> nts.dll
+	#		-> visual studio for wamp
+	#			-> vredist.exe
+	#			-> vcredist.exe 
+	#		-> sql server
+	#			-> sqlncli.msi 
+
+if __name__ == '__main__':
+    main()
